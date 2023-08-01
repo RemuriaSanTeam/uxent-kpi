394f361e83d5bfaadf2d7039a3ffbeff517de1fb
taisei.nagashima <taisei.nagashima@UXENTnoMacBook-Air.local>
2023-08-01 18:18:56 +0900

    API連携

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,11 @@
 <template>
   <h1 class="page-title">メインページ</h1>
   <ul class="flex flex-row justify-center">
-    <li class="category">甘ロリ</li>
-    <li class="category">ゴスロリ</li>
-    <li class="category">クラロリ</li>
-    <li class="category">華ロリ</li>
-    <li class="category">皇子ロリ</li>
+    <li class="category">メンズ</li>
+    <li class="category">レディース</li>
+    <li class="category">ユニセックス</li>
+    <li class="category">ボーイズ</li>
+    <li class="category">ガールズ</li>
   </ul>
   <CardItem />
 </template>
