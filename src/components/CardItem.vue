48bb735664ea065bf11397e31e1965101baa00c8
taisei.nagashima <taisei.nagashima@UXENTnoMacBook-Air.local>
2023-08-02 18:18:25 +0900

    外部API連携の際に使用するエンドポイントの作成

--- src/components/CardItem.vue
+++ src/components/CardItem.vue
@@ -3,7 +3,7 @@
     <div v-for="n in 20" :key="n" class="article col-span-1">
       <div class="card-item">
         <h1 class="text-violet-400">商品名 {{ n }}</h1>
-        <v-img src="images/dummy.png" />
+        <img src="images/dummy.png" />
         <p>商品の詳細</p>
       </div>
     </div>
