fc6161133cd9e9d08a1c49e646300c988a225851
taisei.nagashima <taisei.nagashima@UXENTnoMacBook-Air.local>
2023-08-07 18:23:00 +0900

    page新規追加

---
+++ src/components/InfoList.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="info-detail">
+    <ul>
+      <li>サイト名</li>
+      <li>使用技術</li>
+      <li>作成者</li>
+      <li></li>
+      <li></li>
+    </ul>
+  </div>
+</template>
---
+++ src/pages/info.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1 class="page-title">Infomation</h1>
+  <p>各種情報を記述する</p>
+
+  <CardItem />
+</template>
+
+<script></script>
