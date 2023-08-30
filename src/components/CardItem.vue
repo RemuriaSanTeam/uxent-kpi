344aadc96fdfd635df5546d972e17e8e93391f74
taisei.nagashima <taisei.nagashima@UXENTnoMacBook-Air.local>
2023-08-30 18:04:56 +0900

    pageレイアウト作成

--- src/components/CardItem.vue
+++ src/components/CardItem.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="product-item mx-9 grid grid-cols-3 border border-indigo-600">
     <div v-for="n in 20" :key="n" class="article col-span-1">
-      <div class="card-item">
-        <h1 class="text-violet-400">商品名 {{ n }}</h1>
+      <div class="card-item item-border">
+        <h1 class="text-violet-400 text-center">商品名 {{ n }}</h1>
         <img src="images/dummy.png" />
-        <p>商品の詳細</p>
+        <p class="text-center">商品の詳細</p>
       </div>
     </div>
   </div>
